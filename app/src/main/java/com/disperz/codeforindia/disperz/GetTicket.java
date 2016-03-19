33fbf46136c4362b2140511e32656338021b5e36
Srishti Sengupta <srishti.sengupta@gmail.com>
2016-03-19 14:14:18 +0530

    QR code functionality added

--- app/src/main/java/com/disperz/codeforindia/disperz/GetTicket.java
+++ app/src/main/java/com/disperz/codeforindia/disperz/GetTicket.java
@@ -1,13 +1,17 @@
 package com.disperz.codeforindia.disperz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class GetTicket extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_ticket);
+
+        AndroidBarcodeView view = new AndroidBarcodeView(this);
+
+        setContentView(view);
     }
+
 }
