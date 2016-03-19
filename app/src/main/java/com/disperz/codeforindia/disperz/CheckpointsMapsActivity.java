693522a88bdfaa7804ebf23a77c0824f2244dbb0
Naman <namanspace@gmail.com>
2016-03-19 15:44:15 +0530

    Placed Checkpoints correctly.

--- app/src/main/java/com/disperz/codeforindia/disperz/CheckpointsMapsActivity.java
+++ app/src/main/java/com/disperz/codeforindia/disperz/CheckpointsMapsActivity.java
@@ -12,14 +12,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CheckpointsMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
+        setContentView(R.layout.activity_checkpoints_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -40,29 +40,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng chLocation = new LatLng(28, 77);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(chLocation, 9));
+        Intent intent = getIntent();
+        String place = intent.getStringExtra("PLACE");
 
-        // Add a marker in Sydney and move the camera
-        LatLng rash = new LatLng(28.6143, 77.1998);
-        mMap.addMarker(new MarkerOptions().position(rash).title("Rashtrapati Bhavan")).showInfoWindow();
+        setTitle(place);
 
-        LatLng akshar = new LatLng(28.6125, 77.2272);
-        mMap.addMarker(new MarkerOptions().position(akshar).title("Akshardham"));
+        LatLng chLocation = new LatLng(28.6143, 77.1998);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(chLocation, 17));
 
-        LatLng redfort = new LatLng(28.6560, 77.2410);
-        mMap.addMarker(new MarkerOptions().position(redfort).title("Red Fort"));
+        LatLng check1 = new LatLng(28.614456, 77.197835);
+        mMap.addMarker(new MarkerOptions().position(check1).title("Mughal Garden"));
 
-        LatLng lotus = new LatLng(28.5533, 77.2586);
-        mMap.addMarker(new MarkerOptions().position(lotus).title("Lotus Temple"));
+        LatLng rash = new LatLng(28.614219, 77.200345);
+        mMap.addMarker(new MarkerOptions().position(rash).title("Rashtrapati Bhavan Museum")).showInfoWindow();
+
+        LatLng akshar = new LatLng(28.614342, 77.201957);
+        mMap.addMarker(new MarkerOptions().position(akshar).title("Jaipur Column"));
+
+        LatLng redfort = new LatLng(28.614487, 77.199413);
+        mMap.addMarker(new MarkerOptions().position(redfort).title("Cultural Hall"));
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 
             @Override
             public boolean onMarkerClick(Marker arg0) {
-                Intent intent = new Intent(MapsActivity.this, GetTicket.class);
+
+                Intent intent = new Intent(CheckpointsMapsActivity.this, GetTicket.class);
                 intent.putExtra("PLACE", arg0.getTitle());
-                intent.putExtra("GATE", "Entry");
+
+                if (arg0.getTitle().equals("Mughal Garden")) // if marker source is clicked
+                {
+                    intent.putExtra("GATE", "Gate 1");
+                }
+                if (arg0.getTitle().equals("Rashtrapati Bhavan Museum")) // if marker source is clicked
+                {
+                    intent.putExtra("GATE", "Gate 2");
+                }
+
+                if (arg0.getTitle().equals("Jaipur Column")) // if marker source is clicked
+                {
+                    intent.putExtra("GATE", "Gate 3");
+                }
+
+                if (arg0.getTitle().equals("Cultural Hall")) // if marker source is clicked
+                {
+                    intent.putExtra("GATE", "Gate 4");
+                }
+
                 startActivity(intent);
                 return true;
             }
--- app/src/main/java/com/disperz/codeforindia/disperz/GetTicket.java
+++ app/src/main/java/com/disperz/codeforindia/disperz/GetTicket.java
@@ -1,7 +1,10 @@
 package com.disperz.codeforindia.disperz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class GetTicket extends AppCompatActivity {
 
@@ -10,6 +13,26 @@ public class GetTicket extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         AndroidBarcodeView view = new AndroidBarcodeView(this);
+
+        Intent intent = getIntent();
+        final String name = intent.getStringExtra("PLACE");
+        setTitle(name);
+
+        String gate = intent.getStringExtra("GATE");
+        Toast.makeText(GetTicket.this, "Scan this QR code at " + gate + "!", Toast.LENGTH_LONG).show();
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+
+                Intent intent_1 = new Intent(GetTicket.this, CheckpointsMapsActivity.class);
+                intent_1.putExtra("PLACE", name);
+                startActivity(intent_1);
+            }
+        }, 6000);
 
         setContentView(view);
     }
